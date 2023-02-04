469d59b405bea3fd226666cbb70125521a34b2d1
Aida Enna <ZeroHeLL@GMail.com>
2023-02-04 09:00:29 -0500

    I've officially lost control of my life and it is all Kat's fault.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CookieRunRedeemer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRedeem = new System.Windows.Forms.Button();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbResponse = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRepo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRedeem
+            // 
+            this.btnRedeem.Location = new System.Drawing.Point(12, 294);
+            this.btnRedeem.Name = "btnRedeem";
+            this.btnRedeem.Size = new System.Drawing.Size(90, 23);
+            this.btnRedeem.TabIndex = 0;
+            this.btnRedeem.Text = "Redeem Codes";
+            this.btnRedeem.UseVisualStyleBackColor = true;
+            this.btnRedeem.Click += new System.EventHandler(this.btnRedeem_ClickAsync);
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(12, 22);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(173, 20);
+            this.txtUserID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User ID: (Obtainable via Menu -> Settings -> Info, the ID right under \"User Info\"" +
+    ")";
+            // 
+            // txtCodes
+            // 
+            this.txtCodes.Location = new System.Drawing.Point(12, 59);
+            this.txtCodes.Multiline = true;
+            this.txtCodes.Name = "txtCodes";
+            this.txtCodes.Size = new System.Drawing.Size(173, 235);
+            this.txtCodes.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Codes: (Click Fill Codes to populate)";
+            // 
+            // rtbResponse
+            // 
+            this.rtbResponse.Location = new System.Drawing.Point(191, 22);
+            this.rtbResponse.Name = "rtbResponse";
+            this.rtbResponse.ReadOnly = true;
+            this.rtbResponse.Size = new System.Drawing.Size(311, 322);
+            this.rtbResponse.TabIndex = 5;
+            this.rtbResponse.Text = "";
+            this.rtbResponse.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtbResponse_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Fill Codes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRepo
+            // 
+            this.btnRepo.Location = new System.Drawing.Point(12, 321);
+            this.btnRepo.Name = "btnRepo";
+            this.btnRepo.Size = new System.Drawing.Size(173, 23);
+            this.btnRepo.TabIndex = 7;
+            this.btnRepo.Text = "Github repo/support";
+            this.btnRepo.UseVisualStyleBackColor = true;
+            this.btnRepo.Click += new System.EventHandler(this.btnRepo_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(514, 348);
+            this.Controls.Add(this.btnRepo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbResponse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserID);
+            this.Controls.Add(this.btnRedeem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Cookie Run Code Redeemer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRedeem;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbResponse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRepo;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CookieRunRedeemer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRedeem_ClickAsync(object sender, EventArgs e)
+        {
+            foreach (string code in txtCodes.Lines)
+            {
+                if (String.IsNullOrWhiteSpace(code)) { continue; }
+                DataToSend Data = new DataToSend();
+                Data.email = txtUserID.Text;
+                Data.coupon_code = code.Trim();
+                var json = JsonConvert.SerializeObject(Data);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var url = "https://account.devplay.com/v2/coupon/ck";
+                var client = new HttpClient();
+
+                var response = await client.PostAsync(url, data);
+
+                var result = await response.Content.ReadAsStringAsync();
+                Result JSONResult = JsonConvert.DeserializeObject<Result>(result);
+                rtbResponse.AppendText(code + ":" + Environment.NewLine);
+                switch (JSONResult.code)
+                {
+                    case 20000:
+                        LogSuccess("Success!");
+                        break;
+
+                    case 40006:
+                        LogError("Invalid username?");
+                        return;
+
+                    case 42203:
+                        Log("Code has already been redeemed on this account.");
+                        break;
+
+                    case 42201:
+                        Log("Unknown code 42201, already redeemed?");
+                        break;
+
+                    case 42501:
+                        Log("Code is expired.");
+                        break;
+
+                    default:
+                        Log("Unknown result code: " + JSONResult.code);
+                        break;
+                }
+            }
+        }
+
+        public void LogError(string ErrorMessage)
+        {
+            rtbResponse.SelectionColor = Color.Red;
+            rtbResponse.AppendText("[ERROR] ");
+            rtbResponse.SelectionColor = Color.Black;
+            rtbResponse.AppendText(ErrorMessage + Environment.NewLine);
+        }
+
+        public void LogSuccess(string Message)
+        {
+            rtbResponse.SelectionColor = Color.Green;
+            rtbResponse.AppendText(Message + Environment.NewLine);
+            rtbResponse.SelectionColor = Color.Black;
+        }
+
+        public void Log(string Message)
+        {
+            rtbResponse.AppendText(Message + Environment.NewLine);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Website = "https://cookierunkingdom.fandom.com/wiki/Coupon_Codes";
+            Log("Attempting to autofill codes using the wiki at" + Environment.NewLine + Website + "...");
+            WebClient HTMLDownloader = new WebClient();
+            string HTML = HTMLDownloader.DownloadString(Website);
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(HTML);
+            var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//table[1]/tbody/tr/td");
+            int count = 0;
+            foreach (var node in htmlNodes)
+            {
+                if (node.InnerText.Length < 16) { continue; }
+                string MaybeCode = node.InnerText.Substring(0, 16);
+                if (MaybeCode.Contains(",")) { continue; }
+                if (MaybeCode.ToUpper() == MaybeCode)
+                {
+                    if (count == 0) txtCodes.Clear();
+                    txtCodes.AppendText(MaybeCode + Environment.NewLine);
+                    count++;
+                }
+            }
+            LogSuccess("Done! Added " + count + " code(s)!");
+        }
+
+        private void rtbResponse_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void btnRepo_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Aida-Enna/CookieRunCodeRedeemer");
+        }
+    }
+
+    internal class DataToSend
+    {
+        public string email = "";
+        public string coupon_code = "";
+    }
+
+    public class Result
+    {
+        public int code;
+    }
+}
